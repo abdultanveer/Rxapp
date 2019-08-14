2d3fa5e13293957d307dec021bffad6ae0f4dcdd
abdul <abdul.tanveer@gmail.com>
2019-08-14 22:02:21 +0530

    databinding advanced

--- app/src/main/java/com/example/rxapp/DataBndingAct.java
+++ app/src/main/java/com/example/rxapp/DataBndingAct.java
@@ -16,6 +16,8 @@ public class DataBndingAct extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_data_binding);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_data_binding);
+
+        
         binding.tvHeading.setText("Welcome to JournalDev.com");
         binding.tvSubHeading.setText("Welcome to this Android Tutorial on DataBinding");
 
